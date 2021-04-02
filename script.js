6719bd4a6d36546bb92241ac694c2e1ed9b75ed7
Soumyo78 <soumyo78@yahoo.com>
2021-04-02 17:44:05 +0530

    Image Carousel Added

---
+++ script.js
@@ -0,0 +1,38 @@
+let slideImgArr = [
+    "Resources/slide-pic-9.png",
+    "Resources/slide-pic-10.png",
+    "Resources/slide-pic-11.png",
+    "Resources/slide-pic-12.png",
+    "Resources/slide-pic-13.png",
+    "Resources/slide-pic-14.png"
+];
+
+const autoSlideCarousel = (locationDOM, ImgArr) =>{
+    let offset = 0;
+    const slideImg = (numberOfImg, locationDOM) =>{
+        let rate = 100/numberOfImg;
+        offset = (offset+rate)%100;
+        locationDOM.style.transform = `translate(-${offset}%, 0)`
+    }
+
+    locationDOM.style.width = `${ImgArr.length*100}%`;
+
+    ImgArr.forEach(imgUrl => {
+        const imgContainer = document.createElement('div');
+        imgContainer.style.width = "100%";
+        imgContainer.style.marginLeft = "1px";
+        imgContainer.style.marginRight = "1px";
+
+        const slideImg = document.createElement('img');
+        slideImg.src = imgUrl;
+        slideImg.style.width = "100%";
+
+        imgContainer.appendChild(slideImg);
+        locationDOM.appendChild(imgContainer);
+    });
+
+    setInterval(()=>slideImg(ImgArr.length, carouselContainer), 3000)
+}
+
+const carouselContainer = document.getElementById('app');
+autoSlideCarousel(carouselContainer, slideImgArr); 
